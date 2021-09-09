458ef5ce36f15a75758345c2a50c2f19554b84b3
unknown <shubhambartia44@gmail.com>
2021-09-09 18:47:22 +0530

    updated blog, contact, home page

--- contact_process.php
+++ contact_process.php
@@ -1,11 +1,11 @@
 <?php
 
     $to = "mailto:shubhambartia44@gmail.com";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
+    $from = $_POST['email'];
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $number = $_POST['number'];
+    $cmessage = $_POST['message'];
 
     $headers = "From: $from";
 	$headers = "From: " . $from . "\r\n";
